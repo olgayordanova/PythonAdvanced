0f217d4d7e4b9e3df42edb8b8b8554fc03acdd8c
Olga Yordanova <olga.yordanova.oy@gmail.com>
2021-02-09 05:01:24 +0200

    Update numbers_dictionary.py

--- Error Handling/numbers_dictionary.py
+++ Error Handling/numbers_dictionary.py
@@ -30,29 +30,3 @@ while line != "End":
     line = input ()
 
 print(numbers_dictionary)
-
-
-
-# Uslovie za korekcia
-# numbers_dictionary = {}
-# 
-# line = input()
-#
-# while line != "Search":
-#     number_as_string = line
-#     number = int(input())
-#     numbers_dictionary[number_as_string] = number
-#
-# line = input()
-#
-# while line != "Remove":
-#     searched = line
-#     print(numbers_dictionary[searched])
-#
-# line = input()
-#
-# while line != "End":
-#     searched = line
-#     del numbers_dictionary[searched]
-#
-# print(numbers_dictionary)
